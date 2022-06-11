d777f9acd90181dfda950eaa1911db74acf2a3fd
Alexey Naumov <alexsnaumov@gmail.com>
2022-06-11 14:09:04 +0300

    Analytics dashboard

--- analytics/app/controllers/dashboard_controller.rb
+++ analytics/app/controllers/dashboard_controller.rb
@@ -4,13 +4,42 @@ class DashboardController < ApplicationController
 
   def index
     render locals: {
-      current_balance: rand(0..1000),
-      popugs_with_negative_balance: [],
+      current_balance: calculate_current_balance,
+      popugs_with_negative_balance: collect_popugs_with_debt,
       most_expensive_tasks: {
-        day: rand(0..100),
-        week: rand(0..100),
-        month: rand(0..100),
+        day: find_most_expencive_task(today_transactions),
+        week: find_most_expencive_task(Transaction.week),
+        month: find_most_expencive_task(Transaction.month),
       }
     }
+  end
+
+  private
+
+  def calculate_current_balance
+    calculate_balance(today_transactions)
+  end
+
+  def collect_popugs_with_debt
+    popugs_with_balances = today_transactions.group_by(&:account).map do |account, transactions|
+      transactions_scope = Transaction.where(id: transactions.map(&:id))
+      [account, calculate_balance(transactions_scope)]
+    end
+    popugs_with_balances.select {|_, balance| balance.negative? }
+  end
+
+  def today_transactions
+    @today_transactions ||= Transaction.today
+  end
+
+  def calculate_balance(transactions)
+    income_amount_cents = transactions.debit.sum(:amount_cents).to_i
+    outcome_amount_cents = transactions.credit.sum(:amount_cents).to_i
+    Money.new(income_amount_cents - outcome_amount_cents, 'RUB')
+  end
+
+  def find_most_expencive_task(transactions)
+    amount_cents = transactions.completed_tasks.maximum(:amount_cents).to_i
+    Money.new(amount_cents, 'RUB')
   end
 end
--- analytics/app/models/transaction.rb
+++ analytics/app/models/transaction.rb
@@ -6,5 +6,12 @@ class Transaction < ApplicationRecord
   validates :transaction_type, inclusion: TYPES
   delegate :public_id, to: :account, prefix: true
 
+  scope :today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :week, -> { where(created_at: Time.zone.now.all_week) }
+  scope :month, -> { where(created_at: Time.zone.now.all_month) }
+  scope :debit, -> { where(transaction_type: :debit) }
+  scope :credit, -> { where(transaction_type: :credit) }
+  scope :completed_tasks, -> { credit.where(product_type: :task) }
+
   belongs_to :account
 end
